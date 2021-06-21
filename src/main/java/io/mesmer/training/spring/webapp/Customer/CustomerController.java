16ddf389273604c6bfdf18e65af3e5abb23d30be
WaqasMian <wmian99@gmail.com>
2021-06-21 13:26:30 +0500

    Customer Controller

--- src/main/java/io/mesmer/training/spring/webapp/Customer/CustomerController.java
+++ src/main/java/io/mesmer/training/spring/webapp/Customer/CustomerController.java
@@ -1,7 +1,43 @@
 package io.mesmer.training.spring.webapp.Customer;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/customers")
 public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    // List Customers
+    @RequestMapping("")
+    public List<Customer> getCustomers(){
+        return customerService.getAllCustomers();
+    }
+
+    // Get A Customer
+    @RequestMapping("/{id}")
+    public Customer getCustomer(@PathVariable int id){
+        return customerService.getCustomer();
+    }
+
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void createCustomers(@RequestBody Customer customer){
+        customerService.createCustomer();
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,value="/{id}")
+    public void updateCustomers(@RequestBody Customer customer, @PathVariable int id){
+        customerService.updateCustomer();
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/delete/{id}")
+    public void deleteCustomer(@PathVariable int id){
+        customerService.deleteCustomer();
+    }
+
 }
